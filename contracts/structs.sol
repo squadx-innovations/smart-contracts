0e6ee96bee910a4027453891df8fa8f0722bc117
Squadx Innovations <47051440+squadx-innovations@users.noreply.github.com>
2020-01-20 12:32:00 +0530

    Update structs.sol

--- contracts/structs.sol
+++ contracts/structs.sol
@@ -3,10 +3,18 @@ pragma solidity ^0.4.23;
 contract structures{
       
       string input;
-      
+      uint number;
       function getView(string _name) public view {
           input = _name;
           // comment
       }
+      
+      function setNumber(uint _number) public {
+            number = _number;
+      }
+      
+      function getData() returns(uint) public{
+            return number;
+      }     
   
 }
