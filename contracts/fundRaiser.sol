c5a74109dcab041542f9346f2adc089aa121e09b
Squadx Innovations <47051440+squadx-innovations@users.noreply.github.com>
2020-01-22 13:45:17 +0530

    Added fundraiser contract

--- contracts/fundRaiser.sol
+++ contracts/fundRaiser.sol
@@ -1,6 +1,29 @@
-// changed compiler 
- pragma solidity ^0.4.23;
- 
- contract Funds {
-     
- }
+pragma solidity 0.4.23;
+
+contract FundRaising {
+    address owner = 0x17ef669115293087bc507ddc5e4d6b48209e32d5;
+    uint public total = 0;
+    
+    modifier ownerOnly() {
+        require(msg.sender == owner,"You must be owner");
+        _;
+    }
+    
+    //add funds
+    function add() payable public {
+        require(msg.value > 0, "You need to send some ether");
+        total += msg.value;
+    }
+    
+    //only onwer can withdraw funds
+    function withdrawOwner() public ownerOnly {
+        owner.transfer(address(this).balance);
+        total = 0;
+        assert(this.balance == total);
+    }
+    
+    //total funds collected
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
